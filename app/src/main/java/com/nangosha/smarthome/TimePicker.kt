d794ae912ccde09f4d0c7baf6bfadae69b77dc3b
trevor james nangosha <trevornangosha16@gmail.com>
2023-05-20 16:16:23 +0300

    New formatting for strings that have single chars

--- app/src/main/java/com/nangosha/smarthome/TimePicker.kt
+++ app/src/main/java/com/nangosha/smarthome/TimePicker.kt
@@ -17,12 +17,31 @@ fun timePicker(viewModel: SmartHomeViewModel) {
     val mMinute = mCalendar[Calendar.MINUTE]
 
     val mTimePickerDialog = TimePickerDialog(
-        // todo
-        // apply the custom yellow style to the face of the dialog
-
         mContext,
         { _, mHourr: Int, mMinutee: Int ->
-            val newTime = "$mHourr:$mMinutee"
+            var newTime = ""
+            var hours = ""
+            var minutes = ""
+            hours = if (mHourr < 10){
+                if (mHourr == 0){
+                    "00"
+                } else {
+                    mHourr.toString().padStart(2, '0')
+                }
+            }else{
+                mHourr.toString()
+            }
+
+            minutes = if (mMinutee < 10){
+                if (mMinutee == 0){
+                    "00"
+                } else{
+                    mMinutee.toString().padStart(2, '0')
+                }
+            } else{
+                mMinutee.toString()
+            }
+            newTime = "$hours:$minutes"
             viewModel.updateRoutineTime(newTime)
         }, mHour, mMinute, true
     )
